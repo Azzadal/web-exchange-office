b289e6f13ff0b9ebf08aefc42fe75e26b4706bd2
Azzadal <andr280389@yandex.ru>
2020-01-16 23:32:27 +0700

    WIP

--- src/main/resources/static/js/postrequest.js
+++ src/main/resources/static/js/postrequest.js
@@ -6,27 +6,28 @@ document.getElementById('butBuy').onclick = function (select) {
     if (n === 2) ajaxPostBuy("ERBuy");
     if (n === 3) ajaxPostBuy("UEBuy");
     if (n === 4) ajaxPostBuy("EUBuy");
-    }
+}
 
- document.getElementById('butSell').onclick = function (select) {
-     select = changePair;
-     n = select.selectedIndex;
-     if (n === 1) ajaxPostSell("URSell");
-     if (n === 2) ajaxPostSell("ERSell");
-     if (n === 3) ajaxPostSell("UESell");
-     if (n === 4) ajaxPostSell("EUSell");
- }
+document.getElementById('butSell').onclick = function (select) {
+    select = changePair;
+    n = select.selectedIndex;
+    if (n === 1) ajaxPostSell("URSell");
+    if (n === 2) ajaxPostSell("ERSell");
+    if (n === 3) ajaxPostSell("UESell");
+    if (n === 4) ajaxPostSell("EUSell");
+}
 
 const quantityBuy = document.getElementById('quantityBuy');
 const rateBuy = document.getElementById('rateBuy');
 const totalBuy = document.getElementById('totalBuy');
 const bidsSell = document.getElementById('bidsSell');
 const bidsBuy = document.getElementById('bidsBuy');
-const rowPrice = document.getElementsByClassName('rowPrice');
-const rowQuan = document.getElementsByClassName('rowQuan');
-const rowTotal = document.getElementsByClassName('rowTotal');
-const row = document.getElementsByClassName('row');
- //отправка заявки
+const rowsPriceSell = document.getElementsByClassName('rowsPriceSell');
+const rowsQuanSell = document.getElementsByClassName('rowsQuanSell');
+const rowsTotalSell = document.getElementsByClassName('rowsTotalSell');
+const rowSell = document.getElementsByClassName('rowSell');
+const rowsSell = document.getElementsByClassName('rowsSell');
+//отправка заявки
 function ajaxPostBuy(pair){
     const formData = JSON.stringify({
         rate: $("#rateBuy").val(),
@@ -60,51 +61,49 @@ function ajaxPostBuy(pair){
     $("#totalBuy").val("0");
 }
 
- function ajaxPostSell(pair){
-     const formData = JSON.stringify({
-         rate: $("#rateSell").val(),
-         quantity: $("#quantitySell").val(),
-         total: $("#totalSell").val(),
-         type: pair
-     });
-     const request = new XMLHttpRequest();
-     request.responseType = "json";
-     request.open('POST', window.location + pair);
-     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-     request.onload = function () {
-         if (request.readyState === request.DONE) {
-             if (request.status === 200) {
-                 const json = request.response;
-                 myJson(json);
-                 temp();
-             }
-         }
-     }
-     request.send(formData);
+function ajaxPostSell(pair){
+    const formData = JSON.stringify({
+        rate: $("#rateSell").val(),
+        quantity: $("#quantitySell").val(),
+        total: $("#totalSell").val(),
+        type: pair
+    });
+    const request = new XMLHttpRequest();
+    request.responseType = "json";
+    request.open('POST', window.location + pair);
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.onload = function () {
+        if (request.readyState === request.DONE) {
+            if (request.status === 200) {
+                const json = request.response;
+                myJson(json);
+                autofillBuy();
+            }
+        }
+    }
+    request.send(formData);
+    let i;
+    function myJson(arr){
+        bidsSell.innerHTML = '';
+        for(i = 0; i < arr.length; i++) {
+            bidsSell.innerHTML += '<div class="rowsSell"><div class="rowsPriceSell">' + arr[i].rate + '</div><div class="rowsQuanSell">' + arr[i].quantity + '</div><div class="rowsTotalSell">' +
+                arr[i].total + '</div></div>';
+            rowsSell[i].style.display = "flex";
+        }
+    }
+    function autofillBuy(){
+        for (let i = 0; i < rowsSell.length; i++) {
+            rowsSell[i].addEventListener('click',function () {
+                rateBuy.innerHTML = rowsPriceSell[i].innerHTML;
+                quantityBuy.value = rowsQuanSell[i].innerHTML;
+                outTotal();
+            })
+        }
+    }
 
-     function myJson(arr){
-         let i, n = 0;
-         bidsSell.innerHTML = '';
-         for(i = 0; i < arr.length; i++) {
-             bidsSell.innerHTML += '<div class="row"><div class="rowPrice">' + arr[i].rate + '</div><div class="rowQuan">' + arr[i].quantity + '</div><div class="rowTotal">' +
-                 arr[i].total + '</div></div>';
-             row[i].style.display = "flex";
-         }
-
-     }
-     function temp(){
-         for (let i = 0; i < row.length; i++) {
-             row[i].addEventListener('click',function () {
-                 rateBuy.innerHTML = rowPrice[i].innerHTML;
-                 quantityBuy.value = rowQuan[i].innerHTML;
-                 totalBuy.innerHTML = rowTotal[i].innerHTML;
-             })
-         }
-     }
-
-     $("#quantitySell").val("0");
-     $("#totalSell").val("0");
- }
+    $("#quantitySell").val("0");
+    $("#totalSell").val("0");
+}
 
 function ajaxRate(type) {
     const formData = JSON.stringify({
@@ -118,4 +117,3 @@ function ajaxRate(type) {
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.send(formData);
 }
-
